90e62cfe057a2611ed5f4088cc344fb99557d47b
poseidon1236 <546534112@qq.com>
2015-04-29 11:02:49 +0800

    QR code demo
    QR code demo

---
+++ app/src/main/java/com/android/test/androidtest/MainActivity.java
@@ -0,0 +1,62 @@
+package com.android.test.androidtest;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.Zxing.Demo.CaptureActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resultTextView = (TextView)this.findViewById(R.id.textView2);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void OnClickEvent(View view) {
+        Intent openCameraIntent = new Intent(MainActivity.this, CaptureActivity.class);
+        startActivityForResult(openCameraIntent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            resultTextView.setText(scanResult);
+        }
+    }
+}
